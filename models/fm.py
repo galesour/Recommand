16f2b0d262a3af8a141fdc9ad38c6fce59d8ebce
galesour <765031705@qq.com>
2020-04-23 19:57:30 +0800

    no message

--- models/fm.py
+++ models/fm.py
@@ -1,7 +1,7 @@
 '''
 @Author: 风满楼
 @Date: 2020-04-22 19:57:31
-@LastEditTime: 2020-04-23 19:54:39
+@LastEditTime: 2020-04-23 19:57:22
 @LastEditors: Please set LastEditors
 @Description: 实现FM模型
 @FilePath: /eyepetizer_recommends/recommends/frame_sort/models/fm.py
@@ -19,8 +19,9 @@ if __name__ == "__main__":
     # 前期的数据处理
     data = pd.read_csv('../data_set/criteo_sample.txt')[0:20] # 读取样例数据
     labels = data['label']
-    sparse_features = ['I{}'.format(i) for i in range(1,14)] # 连续特征
-    dense_features = ['C{}'.format(i) for i in range(1,27)] # 离散特征
+    sparse_features = ['C' + str(i) for i in range(1, 27)]
+    dense_features = ['I' + str(i) for i in range(1, 14)]
+
     data[sparse_features] = data[sparse_features].fillna('-1', )
     data[dense_features] = data[dense_features].fillna(0, )
     target = ['label']
@@ -28,11 +29,12 @@ if __name__ == "__main__":
     # 1.Label Encoding for sparse features,and do simple Transformation for dense features
     for feat in sparse_features:
         lbe = LabelEncoder()
-        data[feat] = lbe.fit_transform(data[feat])
+        data[feat] = lbe.fit_transform(data[feat]) #将数据中的离散特征做标准化
+    mms = MinMaxScaler(feature_range=(0, 1))
+    data[dense_features] = mms.fit_transform(data[dense_features]) #将离散数据做归一化
 
-    mns = MinMaxScaler(feature_range=(0, 1))
-    data[dense_features] = mns.fit_transform(data[dense_features])
-    sparse_input_column = [SparseClass(feat_name=feat, voca_size=data[feat].unique()) for feat in sparse_inputs]
+    # 2.count #unique features for each sparse field,and record dense feature field name
+    sparse_input_column = [SparseClass(feat_name=feat, voca_size=data[feat].nunique()) for feat in sparse_inputs]
     dense_input_column = [DenseClass(feat_name=feat) for feat in dense_inputs]
     sparse_input_layers, dense_input_layers = get_input_layer(sparse_input_column + dense_input_column)
     print(sparse_input_layers[0].name)
