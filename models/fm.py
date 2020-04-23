82837aec6683f42159bcb75f86c91a17204a768f
galesour <765031705@qq.com>
2020-04-23 16:38:19 +0800

    no message

--- models/fm.py
+++ models/fm.py
@@ -8,25 +8,28 @@
 '''
 import sys
 sys.path.append('../')
+import pandas as pd 
 from keras import Model
 from keras.layers import Input
 from layers.one_order_layer import OneOrder
 
-
 if __name__ == "__main__":
-    sparse_inputs = [
-        Input(shape=(2,), name='userType'),
-        Input(shape=(2,), name='gender'),
-        Input(shape=(10,), name='category'),
-    ]
-    dense_inputs = [
-        Input(shape=(1,), name='collection_count'),
-        Input(shape=(1,), name='share_count'),
-        Input(shape=(1,), name='play_count'),
-        Input(shape=(1,), name='collect_share_count'),
-    ]
-    y_one_order = OneOrder()([sparse_inputs, dense_inputs])
-    model = Model(inputs=[sparse_inputs, dense_inputs], outputs = y_one_order)
-    model.summary()
+    
+    data = pd.read_csv('data_set/crite_sample.txt')
+    print(data)
+    # sparse_inputs = [
+    #     Input(shape=(2,), name='userType'),
+    #     Input(shape=(2,), name='gender'),
+    #     Input(shape=(10,), name='category'),
+    # ]
+    # dense_inputs = [
+    #     Input(shape=(1,), name='collection_count'),
+    #     Input(shape=(1,), name='share_count'),
+    #     Input(shape=(1,), name='play_count'),
+    #     Input(shape=(1,), name='collect_share_count'),
+    # ]
+    # y_one_order = OneOrder()([sparse_inputs, dense_inputs])
+    # model = Model(inputs=[sparse_inputs, dense_inputs], outputs = y_one_order)
+    # model.summary()
     
     
