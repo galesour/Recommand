4057191729bce8e5688e1d1e9d661e3791e4cedd
galesour <765031705@qq.com>
2020-04-26 11:30:11 +0800

    commit

--- layers/two_order_layer.py
+++ layers/two_order_layer.py
@@ -1,7 +1,7 @@
 '''
 @Author: 风满楼
 @Date: 2020-04-22 20:01:09
-@LastEditTime: 2020-04-25 22:35:41
+@LastEditTime: 2020-04-26 11:30:02
 @LastEditors: Please set LastEditors
 @Description: 实现FM算法的二阶项部分
 @FilePath: /eyepetizer_recommends/recommends/frame_sort/layers/tow_order_layer.py
@@ -28,6 +28,8 @@ class TwoOrder(Layer):
         square_sum = K.sum(all_fields, axis=1) # None * 10 
         print(square_sum.shape)
         res = Subtract()([sum_square, square_sum]) # None * 10
+        print(res.shape)
+        res = K.sum(res, axis=1)
         print(res.shape)
         output = Lambda(lambda x: x*0.5)(res)
         print(output.shape)
