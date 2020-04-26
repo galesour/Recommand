89b5a23538e9aacef45f055b4d7e8dd481e84630
galesour <765031705@qq.com>
2020-04-26 17:36:45 +0800

    commit

--- layers/one_order_layer.py
+++ layers/one_order_layer.py
@@ -1,7 +1,7 @@
 '''
 @Author: 风满楼
 @Date: 2020-04-22 20:00:48
-@LastEditTime: 2020-04-26 11:58:35
+@LastEditTime: 2020-04-26 17:36:05
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /eyepetizer_recommends/recommends/frame_sort/layers/one_order.py
@@ -34,8 +34,10 @@ class OneOrder(Layer):
     def call(self, inputs):
         sparse_inputs, dense_inputs = inputs
         output = 0
+        
         for sparse_input in sparse_inputs:
             output += Embedding(sparse_input.shape[-1], 1)(sparse_input)[:, 0, :]
+            print(output.shape)
         dense_inputs = K.concatenate(dense_inputs)
         tmp = K.dot(dense_inputs, self.dense_weights) # (None, 1)
         output += tmp
