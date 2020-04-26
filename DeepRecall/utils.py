44c269c452de5aef380b087cbca980c6b5a225ec
galesour <765031705@qq.com>
2020-04-26 18:25:20 +0800

    no message

---
+++ DeepRecall/utils.py
@@ -0,0 +1,11 @@
+'''
+@Author: 风满楼
+@Date: 2020-04-26 18:24:20
+@LastEditTime: 2020-04-26 18:25:13
+@LastEditors: Please set LastEditors
+@Description: some util function of deeprecall
+@FilePath: /frame_sort/DeepRecall/utils.py
+'''
+
+if __name__ == "__main__":
+    print('hello world')
