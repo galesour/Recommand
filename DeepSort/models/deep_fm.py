97723eeba3eed384f04f77ab89fedc4b402b1b27
galesour <765031705@qq.com>
2020-04-26 19:18:05 +0800

    commit

--- DeepSort/models/deep_fm.py
+++ DeepSort/models/deep_fm.py
@@ -1,7 +1,7 @@
 '''
 @Author: 风满楼
 @Date: 2020-04-22 19:57:31
-@LastEditTime: 2020-04-26 18:12:21
+@LastEditTime: 2020-04-26 19:17:57
 @LastEditors: Please set LastEditors
 @Description: 实现FM模型
 @FilePath: /eyepetizer_recommends/recommends/frame_sort/models/fm.py
@@ -10,8 +10,8 @@ import sys
 sys.path.append('../')
 import pandas as pd 
 import numpy as np 
-import tensorflow as tf 
-tf.config.experimental_run_functions_eagerly(True)
+# import tensorflow as tf 
+# tf.config.experimental_run_functions_eagerly(True)
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Input, Concatenate
 from tensorflow.keras import backend as K 
@@ -20,7 +20,7 @@ from layers.one_order_layer import OneOrder
 from layers.two_order_layer import TwoOrder
 from layers.LR import Combine
 from layers.dnn_layer import DeepOrder
-from input import SparseClass, DenseClass, get_input_layer, get_embedding_layer
+from models.input import SparseClass, DenseClass, get_input_layer, get_embedding_layer
 
 def get_deep_fm_model(sparse_features, dense_features, data):
     # get the input class
